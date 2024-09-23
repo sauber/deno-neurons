25af472741e06a46c0374440ec974129092571bd
Søren Døssing <netcom@sauber.net>
2024-09-23 16:22:32 +0200

    Example

---
+++ example.ts
@@ -0,0 +1,18 @@
+import { Network, Train, Dashboard } from "jsr:@sauber/neurons";
+
+const network = new Network(2).dense(8).lrelu.dense(6).lrelu.dense(1).tanh;
+const inputs: number[][] = [];
+const outputs: number[][] = [];
+for (let i = 0; i < 100; ++i) {
+  const [x, y] = [Math.random() * 14 - 7, Math.random() * 14 - 7];
+  inputs.push([x, y]);
+  outputs.push([-Math.sin(Math.sqrt(x * x + y * y))]);
+}
+
+const train = new Train(network, inputs, outputs);
+train.epsilon = 0.01;
+const dashboard = new Dashboard(train, 78, 16);
+const max_iteration = 20000;
+const learning_rate = 0.2;
+const redraw = 100;
+dashboard.run(max_iteration, learning_rate, redraw);
